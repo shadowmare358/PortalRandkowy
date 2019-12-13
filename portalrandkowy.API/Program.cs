fab1f265ef24fcf48eca01f863044aa4a86f84c9
Michał Jurczak <shadowmare@windowslive.com>
2019-12-13 12:57:34 +0100

    Utworzenie bazy danych

--- portalrandkowy.API/Program.cs
+++ portalrandkowy.API/Program.cs
@@ -6,21 +6,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore;
 
 namespace portalrandkowy.API
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+       public static void Main(string[] args)
+    {
+        BuildWebHost(args).Run();
+    }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+    public static IWebHost BuildWebHost(string[] args) =>
+    WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>()
+        .Build();
     }
 }
--- portalrandkowy.API/Startup.cs
+++ portalrandkowy.API/Startup.cs
@@ -27,7 +27,7 @@ namespace portalrandkowy.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite("ConnectionString"));
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddMvc(options => {
                 options.EnableEndpointRouting = false;
