80f883488ad4a8e7dc578fb98bd0646723df4ceb
Michał Jurczak <shadowmare@windowslive.com>
2019-12-18 12:24:27 +0100

    1_19) Dodany serwis autoryzacji Angular

--- portalrandkowy.SPA/src/app/nav/nav.component.ts
+++ portalrandkowy.SPA/src/app/nav/nav.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  model: any = {};
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  logIn(){
+    console.log(this.model)
+  }
 }
