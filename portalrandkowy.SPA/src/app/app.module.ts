43c32bce40e87c8ad13e85103ed2a4a00c1f8406
Michał Jurczak <shadowmare@windowslive.com>
2019-12-23 10:38:19 +0100

    1_23) Przekazywanie wartości z komponentu Home do Register

--- portalrandkowy.SPA/src/app/app.module.ts
+++ portalrandkowy.SPA/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
@@ -14,7 +13,6 @@ import { RegisterComponent } from './register/register.component';
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
       NavComponent,
       HomeComponent,
       RegisterComponent
